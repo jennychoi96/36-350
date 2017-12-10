b638ad08ad0c8f867a314b36d641729882666e50
jennychoi96 <bokyungc@andrew.cmu.edu>
2017-12-10 00:07:59 -0500

    Update hello_aliens.R

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,8 +1,8 @@
-Hello aliens!
+cat(Hello aliens!
 
 My name is Jenny from the earth. 
 Here in earth, we have water, food and people called human!
 I would like to know what you call yourselves.
 I hope this message is received. Please reply. Thanks!
 
-Jenny
+Jenny)
